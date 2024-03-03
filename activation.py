9412b885be4ed25351ac9c31b874324d4b7e1786
Tianyi Xie <tianyixie96@outlook.com>
2024-03-02 17:11:19 -0800

    Implement activation function sigmoid and softmax.

--- activation.py
+++ activation.py
@@ -1,13 +1,21 @@
-class Sigmoid:
-    def call(self, input):
-        pass
+import numpy as np
 
-    def derivative(self, input):
-        pass
+class Sigmoid:
+    def call(self, x):
+        sigma_x = 1 / (1 + np.exp(-x))
+        return sigma_x
+
+    def derivative(self, x):
+        sigma_x = self.call(x)
+        d_sigma_x = sigma_x * (1 - sigma_x)
+        return d_sigma_x
 
 class Softmax:
-    def call(self, input):
-        pass
+    def call(self, x):
+        exp_x = np.exp(x)
+        softmax_x = exp_x / np.sum(exp_x, axis=0)
+        return softmax_x
 
     def derivative(self, input):
+
         pass
